3aa69caff5ca99659313cb94834020645eb723a8
ddogga <hayeonlee60327@gmail.com>
2023-03-09 18:25:12 +0900

    initialize vue project

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+
+  outputDir:
+    "C:/Users/User/Desktop/Programing/WorkSpace/project/study/tutoring/shop/src/main/resources/static",
+
+  //index.html경로
+  indexPath:
+    "C:/Users/User/Desktop/Programing/WorkSpace/project/study/tutoring/shop/src/main/resources/templates/index.html",
+
+  devServer: {
+    port: 8085,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8084",
+        changeOrigin: true, // cross origin 허용
+      },
+    },
+  },
+});
