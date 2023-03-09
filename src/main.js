51c7bb50c5310b852919ffcad085692555b19478
ddogga <hayeonlee60327@gmail.com>
2023-03-09 23:28:13 +0900

    feat : import js,css files

--- src/main.js
+++ src/main.js
@@ -4,10 +4,8 @@ import App from "./App.vue";
 // ==========================================
 // Style
 // ==========================================
-import "@/assets/vendor/fontawesome-free/css/all.min.css";
 import "@/assets/css/sb-admin-2.min.css";
-// ==========================================
-// js
-// ==========================================
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.mount("#app");
